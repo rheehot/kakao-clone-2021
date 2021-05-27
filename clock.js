6e4f0768bc52a7f731137a217527fbcc861a4924
djdu4496 <djdu4496@gmail.com>
2021-05-27 20:38:57 +0900

    clock up-date

--- clock.js
+++ clock.js
@@ -1,20 +1,20 @@
-const clockContainer = document.querySelector(".status-bar");
-const clockColumn = clockContainer.querySelector(".status-bar__column");
-const clockTitle = clockColumn.querySelector("#clock");
+const clockContainer = document.querySelector(".status-bar"),
+    clockTitle = clockContainer.querySelector(".status-bar__column.time"),
+    clock = clockTitle.querySelector("span");
 
 
 function getTime() {
     const date = new Date();
     const minutes = date.getMinutes();
     const hours = date.getHours();
-    const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds
-    }`;
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+        minutes < 10 ? `0${minutes}` : minutes
+      }`;
 }
 
 function init() {
     getTime();
-    setInterval(getTime,1000)
+    setInterval(getTime,1000);
 }
 
 init();
